b76bae2b629a7ed9b17b87b76613d754d5ef6355
Miquéias Veloso <84027511+miqueiasveloso@users.noreply.github.com>
2024-04-10 06:23:50 -0300

    Update app.js

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ app.get('/', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
